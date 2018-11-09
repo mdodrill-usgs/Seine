a703890998a72c6787f199f4f7159ac3b36b4418
Mike <mdodrill@usgs.gov>
2018-11-09 11:44:08 -0700

    Cleaned up plotting for p

--- Code/Plotting_v1.R
+++ Code/Plotting_v1.R
@@ -1,5 +1,5 @@
 ###############################################################################
-#                                                                        Aug 18
+#                                                                        Nov 18
 #           JAGS Model Fitting - 2 Stage MultSp Removal Model 
 #
 #  Notes:
@@ -14,31 +14,12 @@ library(ggmcmc)
 library(ggthemes)
 library(dplyr)
 
+# library(devtools)
+# install_github(repo = 'mdodrill-usgs/fishR')
+
+library(fishR)
+
 windows(record = T, xpos = 25, width = 12, height = 7)
-#-----------------------------------------------------------------------------#
-
-par.name = "p"
-
-tmp = coda::mcmc.list(lapply(1:fit$model$nchain(), function(x) coda::mcmc(fit$BUGSoutput$sims.array[,x,])))
-
-f1 = ggs(tmp, family = par.name)
-
-
-# f1 = coda::mcmc.list(lapply(1:fit$model$nchain(), function(x) coda::mcmc(fit$BUGSoutput$sims.array[,x,])))
-
-
-sub = f1[f1$Parameter == "mean.p",]
-sub2 = f1[f1$Parameter != "mean.p",]
-
-
-p = ggplot(sub2, aes(x = value, group = Parameter)) +
-  # geom_density(aes(color = Parameter, fill = Parameter), alpha = .2) +
-  geom_density(color = "gray70", fill = "dodgerblue2", alpha = .2) +
-  geom_density(data = sub, fill = "black", alpha = .5) +
-  theme_base()
-p
-
-
 #-----------------------------------------------------------------------------#
 sp.key = data.frame(name = unique(d1_r$species),
                     num = 1:Nsp)
@@ -46,37 +27,25 @@ sp.key = data.frame(name = unique(d1_r$species),
 sp.key.big = data.frame(name = d1_r$species,
                         num = 1:Nsamps_r)
 
+#-----------------------------------------------------------------------------#
+# individual p's for all site, species
 
+p.dat = organize(fit = fit, par.name = "p")
 
-name = "mean.p"
-name2 = "p["
+p.dat$id = as.numeric(gsub("[^0-9]", "", as.character(p.dat$Parameter)))
 
-tmp = coda::mcmc.list(lapply(1:fit$model$nchain(), function(x) coda::mcmc(fit$BUGSoutput$sims.array[,x,])))
+p.dat$species = sp.key.big[match(p.dat$id, sp.key.big$num),]$name
 
-f1 = ggs(tmp)
+sm.p.dat = sample_n(f2.sub, 20000) # cut data down for faster plotting
 
+p = ggplot(p.dat, aes(x = value, group = Parameter)) +
+  geom_density(color = "gray70", fill = "dodgerblue2", alpha = .2) +
+  theme_base()
+p
 
-f1.sub = f1[which(substr(f1$Parameter,1,nchar(name)) == name),]
-
-
-f1.sub$id = as.numeric(gsub("[^0-9]", "", as.character(f1.sub$Parameter)))
-
-f1.sub$species = sp.key[match(f1.sub$id, sp.key$num),]$name
-
-
-f2.sub = f1[which(substr(f1$Parameter,1,nchar(name2)) == name2),]
-
-
-f2.sub$id = as.numeric(gsub("[^0-9]", "", as.character(f2.sub$Parameter)))
-
-f2.sub$species = sp.key.big[match(f2.sub$id, sp.key.big$num),]$name
-
-dat = sample_n(f2.sub, 20000) # cut data down for faster plotting
-
-
-p = ggplot(dat, aes(x = value, group = Parameter)) +
+# facet by species
+p = ggplot(sm.p.dat, aes(x = value, group = Parameter)) +
   geom_density(aes(color = species, fill = species), alpha = .2) +
-  geom_density(data = f1.sub, aes(x = value), color = "black", fill = "black", alpha = .2) +
   facet_wrap(~ species, scales = "free_y") +
   theme_base() +
   theme(legend.position = c(.85,.15)) +
@@ -84,19 +53,34 @@ p = ggplot(dat, aes(x = value, group = Parameter)) +
 p
 
 
+#-----------------------------------------------------------------------------#
+# mean p's for each species 
 
-p = ggplot(f1.sub, aes(x = value)) +
+mu.p.dat = organize(fit = fit, par.name = "mean.p")
+
+mu.p.dat$id = as.numeric(gsub("[^0-9]", "", as.character(mu.p.dat$Parameter)))
+
+mu.p.dat$species = sp.key[match(mu.p.dat$id, sp.key$num),]$name
+
+
+p = ggplot(mu.p.dat, aes(x = value, group = Parameter)) +
   geom_density(aes(color = species, fill = species), alpha = .2) +
-  # geom_density(data = f1.sub, aes(x = value), color = "black", fill = "black", alpha = .2) +
-  # facet_wrap(~ species, scales = "free_y") +
   theme_base() +
   theme(legend.position = c(.15,.85)) +
   guides(col = guide_legend(ncol = 3))
 p
 
+#-----------------------------------------------------------------------------#
+# both the mean.p and individual p's, facet by species 
 
-
-
+p = ggplot(sm.p.dat, aes(x = value, group = Parameter)) +
+  geom_density(aes(color = species, fill = species), alpha = .2) +
+  geom_density(data = mu.p.dat, aes(x = value), color = "black", fill = "black", alpha = .2) +
+  facet_wrap(~ species, scales = "free_y") +
+  theme_base() +
+  theme(legend.position = c(.85,.15)) +
+  guides(col = guide_legend(ncol = 3))
+p
 
 #-----------------------------------------------------------------------------#
 name = "Ncat"
